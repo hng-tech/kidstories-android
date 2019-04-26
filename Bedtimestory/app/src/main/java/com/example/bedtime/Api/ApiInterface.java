a7e1e82e77921786b9d548f745d37e66b9b37e26
frankarinze <arinze086@gmail.com>
2019-04-26 19:11:32 +0100

    Revert "Production"

--- Bedtimestory/app/src/main/java/com/example/bedtime/Api/ApiInterface.java
+++ Bedtimestory/app/src/main/java/com/example/bedtime/Api/ApiInterface.java
@@ -11,17 +11,12 @@ import com.example.bedtime.Model.User;
 
 import java.util.List;
 
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface ApiInterface {
@@ -50,12 +45,4 @@ public interface ApiInterface {
 
     @GET("user/profile/{id}")
     Call<User> getProfile(@Path("id") String id);
-
-    @Multipart
-    @POST("story/create")
-    Call<ResponseBody> addStory(
-            @Part("title") RequestBody title,
-            @Part("story") RequestBody story,
-            @Part MultipartBody.Part image
-    );
 }
