9741950b2ec2549aee76cbec6634b865a18a3be9
frankarinze <arinze086@gmail.com>
2019-05-05 01:16:21 +0100

    Bottom Menu Fragment class fix

---
+++ Bedtimestory/app/src/main/java/com/dragonlegend/kidstories/Fragments/BottomMenuFragment.java
@@ -0,0 +1,27 @@
+package com.dragonlegend.kidstories.Fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dragonlegend.kidstories.R;
+
+public class BottomMenuFragment extends BottomSheetDialogFragment {
+    public BottomMenuFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.bottom_menu_dialog, container, false);
+    }
+}
